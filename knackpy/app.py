674f9fa80a9d50283d07325d2dcf7b3b143f1f74
johnclary <john.clary@austintexas.gov>
2020-05-27 08:26:13 -0500

    handle names as keys

--- knackpy/app.py
+++ knackpy/app.py
@@ -13,6 +13,7 @@ class App:
     """
     Knack application wrapper. This thing does it all, folks!
     """
+
     def __repr__(self):
         info_str = ", ".join([f"{value} {key}" for key, value in self.info.items()])
         return f"""<App [{self.metadata["name"]}]> ({info_str})"""
@@ -30,6 +31,7 @@ class App:
         self.session = KnackSession(self.app_id, self.api_key, timeout=timeout)
         self.metadata = self._get_metadata()
         self.view_lookup = self._generate_view_lookup()
+        self.obj_lookup = self._generate_obj_lookup()
         self.info = self._parse_app_info()
         logging.debug(self)
 
@@ -53,43 +55,82 @@ class App:
 
     def _generate_view_lookup(self):
         return {
-            view["key"]: {"scene": scene["key"]}
+            view["key"]: {"scene": scene["key"], "name": view["name"]}
             for scene in self.metadata["scenes"]
             for view in scene["views"]
         }
 
-    def _validate_key(self, key):
-        obj_keys = [obj["key"] for obj in self.metadata["objects"]]
-        view_keys = list(self.view_lookup.keys())
+    def _generate_obj_lookup(self):
+        return {obj["key"]: {"name": obj["name"]} for obj in self.metadata["objects"]}
 
-        if key in obj_keys:
-            return {"key": key, "type": "object"}
+    def _route(self, key):
+        """
+        Get the API route for an view key, and object key, an object name, or a view name.
 
-        if key in view_keys:
-            return {"key": key, "type": "view", "scene": self.view_lookup[key]["scene"]}
+        If using names instead of keys, the client is responsibile for worrying about namespace conflicts.
+        """
+        try:
+            return self._object_route(key)
 
-        raise ValidationError(
-            f"Unknown Knack key supplied. `{key}` not in {{ {', '.join([key for key in obj_keys + view_keys]) } }}"
-        )
+        except IndexError:
+            pass
 
-    def _route(self, key=None, type_=None, scene=None):
-        if type_ == "object":
-            return f"/objects/{key}/records"
-        else:
+        try:
+            return self._view_route(key)
+
+        except (IndexError, KeyError):
+            raise ValidationError(f"Unknown Knack key supplied: `{key}`")
+
+    def _view_route(self, key):
+        try:
+            # try to find a matching view key
+            scene = self.view_lookup[key]
             return f"/pages/{scene}/views/{key}/records"
+
+        except KeyError:
+            pass
+
+        try:
+            # try to find a matching view name
+            match = [
+                (value["scene"], view_key)
+                for view_key, value in self.view_lookup.items()
+                if value["name"] == key
+            ]
+            return f"/pages/{match[0][0]}/views/{match[0][1]}/records"
+
+        except IndexError as e:
+            raise e
+
+    def _object_route(self, key):
+        try:
+            # try to find a matching object key
+            match = self.obj_lookup[key]
+            return f"/objects/{key}/records"
+        except KeyError:
+            pass
+
+        try:
+            # try to find a matching object name
+            match = [
+                obj_key
+                for obj_key, value in self.obj_lookup.items()
+                if value["name"] == key
+            ]
+            return f"/objects/{match[0]}/records"
+
+        except IndexError as e:
+            raise e
 
     def get_data(self, *keys, **kwargs):
         """
         *keys: each arg must be an object or view key string that exists in the app
         **kwargs: supported kwargs are record_limit (type: int) and max_attempts (type: int). others are ignored.
         """
-        todos = [self._validate_key(key) for key in keys]
-        self.data_raw = {}
+        self.data = {}
 
-        for key_dict in todos:
-            route = self._route(**key_dict)
-            self.data_raw[key_dict["key"]] = self.session._get_paginated_records(
-                route, **kwargs
-            )
+        for key in keys:
+            route = self._route(key)
+            self.data[key] = self.session._get_paginated_data(route, **kwargs)
 
         return None
