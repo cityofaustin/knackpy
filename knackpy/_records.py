c769f4c718f8877d3d5a93b81e599ed007b2fc61
johnclary <john.clary@austintexas.gov>
2020-05-29 07:53:24 -0500

    records.to_csv()

--- knackpy/_records.py
+++ knackpy/_records.py
@@ -1,3 +1,9 @@
+import csv
+import warnings
+
+from knackpy.utils.utils import _valid_name
+
+
 class Records:
     def __init__(self, data, field_defs):
         self.data = data
@@ -30,13 +36,9 @@ class Records:
             # proxy the raw key as the key
             key = key.replace("_raw", "")
 
-            try:
-                field_def = self.field_defs[key]
-            except KeyError:
-                continue
+            field_def = self.field_defs[key]
 
-            if format_keys:
-                key = field_def.name
+            key = self._handle_key(field_def, format_keys)
 
             handled_record[key] = self._handle_field(value, field_def, format_values)
 
@@ -44,3 +46,40 @@ class Records:
 
     def _handle_field(self, value, field_def, format_values):
         return value if not format_values else field_def.formatter(value)
+
+    def _handle_key(self, field_def, format_keys):
+        if field_def.key == "id" or not format_keys:
+            return field_def.key
+
+        return _valid_name(field_def.name)
+
+    def to_csv(
+        self, *keys, path="", delimiter=",", format_keys=False, format_values=False
+    ):
+        keys = keys if keys else list(self.keys())
+
+        for key in keys:
+            fieldnames = self._get_fieldnames(key, format_keys)
+
+            if not fieldnames:
+                warnings.warn(f"No records found in '{key}'")
+                continue
+
+            records = self.get(
+                key, format_keys=format_keys, format_values=format_values
+            )
+
+            fname = f"{key}.csv"
+
+            with open(fname, "w") as fout:
+                writer = csv.DictWriter(
+                    fout, fieldnames=fieldnames, delimiter=delimiter
+                )
+                writer.writeheader()
+                for record in records:
+                    writer.writerow(record)
+
+    def _get_fieldnames(self, key, format_keys):
+        records = self.get(key, format_keys)
+        for record in records:
+            return record.keys()
