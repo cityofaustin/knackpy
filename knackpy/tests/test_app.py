59176ca29c674e8236f7cc4440f694df99021f26
johnclary <john.clary@austintexas.gov>
2020-05-29 12:38:09 -0500

    yay tests

---
+++ knackpy/tests/test_app.py
@@ -0,0 +1,32 @@
+import json
+import pytest
+import knackpy
+
+@pytest.fixture
+def app_data():
+    with open("tests/_metadata.json", "r") as fin:
+        metadata = json.loads(fin.read())
+        metadata = metadata["application"]
+
+    with open("tests/_all_fields.json", "r") as fin:
+        data = json.loads(fin.read())
+        data = data["records"]
+
+    return { "data": data, "metadata": metadata }
+
+@pytest.fixture
+def app(app_data):
+    return knackpy.App(app_id=app_data["metadata"]["id"], metadata=app_data["metadata"])
+
+def test_constructor_success(app_data):
+    app = knackpy.App(app_id=app_data["metadata"]["id"], metadata=app_data["metadata"])
+    assert app
+
+def test_constructor_generate_records(app, app_data):
+    app.data = {"all_fields_test": app_data["data"]}
+    app.generate_records()
+    assert(app.records)
+
+def test_constructor_fail_missing_app_id(app, app_data):
+    with pytest.raises(TypeError):
+        app = knackpy.App()
