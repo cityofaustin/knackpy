7e251b4a44d175f683c0b8228599336252d6cf18
johnclary <john.clary@austintexas.gov>
2020-05-31 01:22:31 -0500

    stop handling for empty strings, format datetime

--- knackpy/_fields.py
+++ knackpy/_fields.py
@@ -2,8 +2,6 @@ from datetime import datetime, timezone
 import logging
 import pdb
 
-import pytz
-
 from knackpy.exceptions.exceptions import ValidationError
 
 
@@ -38,27 +36,16 @@ class Formatter:
         - timer  x
         - user_roles
         """
-        return None if value == "" else value
+        return value
 
     def email(self, value):
-        try:
-            return value.get("email")
-
-        except AttributeError:
-            return None
+        return value.get("email")
 
     def link(self, value):
-        try:
-            return value.get("url")
-
-        except AttributeError:
-            return None
+        return value.get("url")
 
     def phone(self, value):
-        try:
-            return value.get("full")
-        except AttributeError:
-            return None
+        return value.get("full")
 
     def image(self, value):
         # somtimes a dict, sometimes a str
@@ -66,30 +53,17 @@ class Formatter:
             return value["url"]
         except TypeError:
             return value
-        except AttributeError:
-            return None
 
-    def date_time(self, value):
-        try:
-            mills_timestamp = value.get("unix_timestamp")
-        except AttributeError:
-            return None
-
-        # create naive datetime data instance
-        dt = datetime.fromtimestamp(mills_timestamp / 1000)
-        # make it timezone-aware
-        dt = dt.replace(tzinfo=timezone.utc)
-        # return ISO-formatted str
-        return dt.isoformat()
+    @staticmethod
+    def date_time(value, tz=None):
+        mills_timestamp = value.get("unix_timestamp")
+        timestamp = mills_timestamp / 1000
+        dt = datetime.fromtimestamp(timestamp, tz=timezone.utc)
+        return dt.astimezone(tz).isoformat()
 
     def connection(self, value):
-        try:
-            vals = [val["identifier"] for val in value]
-            return vals if vals else None
-
-        except AttributeError:
-            return None
-
+        vals = [val["identifier"] for val in value]
+        return vals if vals else None
 
 class FieldDef:
     """ Knack field defintion wrapper """
@@ -132,11 +106,10 @@ class FieldDef:
         pass
 
     @staticmethod
-    def correct_knack_timestamp(knack_date_time_dict, timezone):
+    def correct_knack_timestamp(mills_timestamp, tz):
         """
-        Receive a knack_date_time_dict (type: dict) and and pytz timezone and
-        return same dict with a (naive) unix milliseconds timestamp value as
-        `unix_timestamp`.
+        Receive a knack mills timestamp (type: int) and and pytz timezone and
+        return a (naive) unix milliseconds timestamp int.
 
         You may be wondering why timezone settings are concern, given that Knackpy, like the
         Knack API, returns timestamp values as Unix timestamps in millesconds (thus, there is
@@ -146,26 +119,18 @@ class FieldDef:
         For example, if you inspect a timezone value in Knack, e.g., 1578254700000, this value
         represents Sunday, January 5, 2020 8:05:00 PM **local time**.
         """
-        try:
-            mills_timestamp = knack_date_time_dict.get("unix_timestamp")
-        except AttributeError:
-            # knack_date_time_dict is not a dict, so it's almost definitely an empty
-            # string (which Knack uses instead of `null`)
-            return knack_date_time_dict
-
         timestamp = mills_timestamp / 1000
         # Don't use datetime.utcfromtimestamp()! this will assume the input timestamp is in local (system) time
         # If you try to pass our timezone to the tz parameter here, it will have no affect. Ask Guido why??
-        dt_utc = datetime.fromtimestamp(timestamp, tz=pytz.UTC)
+        dt_utc = datetime.fromtimestamp(timestamp, tz=timezone.utc)
         # All we've done so far is create a datetime object from our timestamp
         # now we have to remove the timezone info that we supplied
         dt_naive = dt_utc.replace(tzinfo=None)
         # Now we localize (i.e., translate) the datetime object to our local time
         # you cannot use datetime.replace() here, because it does not account for
         # daylight savings time. I know, this is completely insane. 
-        dt_local = timezone.localize(dt_naive)
+        dt_local = tz.localize(dt_naive)
         # Now we can convert our datetime object back to a timestamp
         unix_timestamp = dt_local.timestamp()
         # And add milliseconds
-        knack_date_time_dict["unix_timestamp"] = int(unix_timestamp * 1000)
-        return knack_date_time_dict
+        return int(unix_timestamp * 1000)
