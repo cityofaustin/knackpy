f1074aa5452126efaa1440d2dcd50c5854c31ccd
johnclary <john.clary@austintexas.gov>
2020-06-03 06:53:27 -0500

    better imports, and now timer handling

--- knackpy/utils/formatters.py
+++ knackpy/utils/formatters.py
@@ -1,6 +1,5 @@
-from datetime import datetime, timezone
+import datetime
 import pdb
-
 
 def default(value):
     """ Handles types:
@@ -22,11 +21,9 @@ def default(value):
     - rich_text
     - short_text  x
     - sum
-    - timer  x
     - user_roles
     """
     return value
-
 
 def signature(value):
     return value.get("base30")
@@ -34,14 +31,11 @@ def signature(value):
 def email(value):
     return value.get("email")
 
-
 def link(value):
     return value.get("url")
 
-
 def phone(value):
     return value.get("full")
-
 
 def image(value):
     # somtimes a dict, sometimes a str
@@ -50,9 +44,12 @@ def image(value):
     except TypeError:
         return value
 
-
-def date_time(value, tz):
+def date_time(value, timezone=None):
     mills_timestamp = value.get("unix_timestamp")
     timestamp = mills_timestamp / 1000
-    dt = datetime.fromtimestamp(timestamp, tz=timezone.utc)
-    return dt.astimezone(tz).isoformat()
+    dt = datetime.datetime.fromtimestamp(timestamp, tz=datetime.timezone.utc)
+    return dt.astimezone(timezone).isoformat()
+
+def timer(value):
+    # we're handling somthing that looks like this: '<span>09/11/19</span>&nbsp;4:14pm to 5:14pm = 1:00 hours'
+    return value.replace("<span>", "").replace("</span>", "").replace("&nbsp;", "; ")
