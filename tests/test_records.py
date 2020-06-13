a7aece378e87761723c694e3bc11fa75d562cb98
John Clary <john.clary@austintexas.gov>
2020-06-13 16:56:16 -0500

    tests for  class

--- tests/test_records.py
+++ tests/test_records.py
@@ -8,6 +8,11 @@ OBJ_NAME_KEY = "all_fields_test"
 
 @pytest.fixture
 def app():
+    """
+    Records are not dependent on the App class, but App wraps a bunch of helpful methods
+    that get us to the point of generating a record. So we use it as a fixture rather than 
+    duplicating that code here.
+    """
     with open("tests/_metadata.json", "r") as fin:
         metadata = json.loads(fin.read())
 
@@ -19,6 +24,20 @@ def app():
     app.data = {OBJ_NAME_KEY: data}
     return app
 
+@pytest.fixture
+def record(app):
+    """
+    You will notice elsewhere in the tests that we use a fixture that is identical
+    to a unit test. i'm not sure how to gracefully accomplish this in tests:
+    - test the basic construction of a class
+    - test the public methods of a class
+    """
+    data = app.data.get(OBJ_NAME_KEY)[0]
+    identifier = "field_6"
+    field_defs = app.field_defs
+    timezone = app.timezone
+    return knackpy._records.Record(data, field_defs, identifier, timezone)
+
 def test_record_constructor(app):
     data = app.data.get(OBJ_NAME_KEY)[0]
     identifier = "field_6"
@@ -26,46 +45,6 @@ def test_record_constructor(app):
     timezone = app.timezone
     assert knackpy._records.Record(data, field_defs, identifier, timezone)
 
-# @pytest.fixture
-# def record_collection(app):
-#     return knackpy._records.RecordCollection(app.data, app.field_defs, app.tz)
-
-# def test_constructor_success(app):
-#     assert knackpy._records.RecordCollection(app.data, app.field_defs, app.tz)
-
-# def test_constructor_fail(app):
-#     with pytest.raises(TypeError):
-#         knackpy._records.RecordCollection()
-
-# def test_get(record_collection):
-#     recs = record_collection.get(KEY)
-#     assert isinstance(recs, types.GeneratorType)
-
-# def test_handle_records(app, record_collection):
-#     record = app.data[KEY][0]
-#     handled = record_collection._handle_record(record, False, True)
-#     assert isinstance(handled, dict)
-
-# def test_handle_records_no_format_keys(app, record_collection):
-#     record = app.data[KEY][0]
-#     handled = record_collection._handle_record(record, False, False)
-#     assert set(record.keys()).issuperset(set(handled.keys()))
-
-# def test_handle_records_format_keys(app, record_collection):
-#     record = app.data[KEY][0]
-#     fieldnames = [value.name for key, value in app.field_defs.items()]
-#     handled = record_collection._handle_record(record, True, False)
-#     handled.pop("_id")  # ignore conflict-resovled `id` fieldname
-#     assert set(fieldnames).issuperset(set(handled.keys()))
-
-# def test_handle_records_no_format_values(app, record_collection):
-#     record = app.data[KEY][0]
-#     record_vals = record.values()
-#     handled_vals = record_collection._handle_record(record, False, False).values()
-#     assert all([val in record_vals for val in handled_vals])
-
-# def test_handle_records_no_format(app, record_collection):
-#     record = app.data[KEY][0]
-#     record_vals = record.values()
-#     handled_vals = record_collection._handle_record(record, False, False).values()
-#     assert all([val in record_vals for val in handled_vals])
+def test_record_format(record):
+    assert record.format()
+    
