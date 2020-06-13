a28fec6c39a39761279fc368ecc8fae386a4ab3b
John Clary <john.clary@austintexas.gov>
2020-06-13 13:20:14 -0500

    hello utils tests

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -13,11 +13,6 @@ def metadata():
 
 
 @pytest.fixture
-def app(metadata):
-    return knackpy.App(app_id=metadata["id"], metadata=metadata)
-
-
-@pytest.fixture
 def container_index(metadata):
     return knackpy.utils.utils.generate_container_index(metadata)
 
@@ -46,8 +41,10 @@ def test_query_container_index_keyerror(container_index):
     with pytest.raises(KeyError):
         assert container_index["Pizza the Hut"]
 
+
 def test_correct_knack_timestamp():
     local_timestamp = 1568218440000  #  Sep 11, 2019 16:14pm UTC
     tz = pytz.timezone("US/Central")
     unix_timestamp = knackpy.utils.utils.correct_knack_timestamp(local_timestamp, tz)
-    assert unix_timestamp ==  1568236440000 # Sep 11, 2019 16:14pm US/Central
+    assert unix_timestamp == 1568236440000  # Sep 11, 2019 16:14pm US/Central
+
