66f544be76620dd4400ff10c353f58b4709b12f1
John Clary <john.clary@austintexas.gov>
2020-06-30 17:26:48 -0500

    refactor to match package changes duh

--- tests/test_records.py
+++ tests/test_records.py
@@ -23,45 +23,23 @@ def app():
 
 
 @pytest.fixture
-def record_collection(app):
-    return knackpy._records.RecordCollection(app.data, app.field_defs, app.tz)
+def records(app):
+    return knackpy._records.Records(
+        "object_3", app.data[KEY], app.field_defs, app.tzinfo
+    )
+
 
 def test_constructor_success(app):
-    assert knackpy._records.RecordCollection(app.data, app.field_defs, app.tz)
+    assert knackpy._records.Records(
+        "object_3", app.data[KEY], app.field_defs, app.tzinfo
+    )
+
 
 def test_constructor_fail(app):
     with pytest.raises(TypeError):
-        knackpy._records.RecordCollection()
+        knackpy._records.Records()
 
-def test_get(record_collection):
-    recs = record_collection.get(KEY)
+
+def test_get(records):
+    recs = records.records()
     assert isinstance(recs, types.GeneratorType)
-
-def test_handle_records(app, record_collection):
-    record = app.data[KEY][0]
-    handled = record_collection._handle_record(record, False, True)
-    assert isinstance(handled, dict)
-
-def test_handle_records_no_format_keys(app, record_collection):
-    record = app.data[KEY][0]
-    handled = record_collection._handle_record(record, False, False)
-    assert set(record.keys()).issuperset(set(handled.keys()))
-
-def test_handle_records_format_keys(app, record_collection):
-    record = app.data[KEY][0]
-    fieldnames = [value.name for key, value in app.field_defs.items()]
-    handled = record_collection._handle_record(record, True, False)
-    handled.pop("_id")  # ignore conflict-resovled `id` fieldname
-    assert set(fieldnames).issuperset(set(handled.keys()))
-
-def test_handle_records_no_format_values(app, record_collection):
-    record = app.data[KEY][0]
-    record_vals = record.values()
-    handled_vals = record_collection._handle_record(record, False, False).values()
-    assert all([val in record_vals for val in handled_vals])
-
-def test_handle_records_no_format(app, record_collection):
-    record = app.data[KEY][0]
-    record_vals = record.values()
-    handled_vals = record_collection._handle_record(record, False, False).values()
-    assert all([val in record_vals for val in handled_vals])
