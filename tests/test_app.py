c2ff57fd1c0c35654d1576240c5c7cc3e8e8e4e3
John Clary <john.clary@austintexas.gov>
2020-07-02 21:30:09 -0500

    drop metadata accessor

--- tests/test_app.py
+++ tests/test_app.py
@@ -8,7 +8,6 @@ import knackpy
 def app_data():
     with open("tests/_metadata.json", "r") as fin:
         metadata = json.loads(fin.read())
-        metadata = metadata["application"]
 
     with open("tests/_all_fields.json", "r") as fin:
         data = json.loads(fin.read())
@@ -20,14 +19,13 @@ def app_data():
 @pytest.fixture
 def app(app_data):
     knackpy_app = knackpy.App(
-        app_id=app_data["metadata"]["id"], metadata=app_data["metadata"]
+        app_id=app_data["metadata"]["application"]["id"], metadata=app_data["metadata"]
     )
     app.data = {"all_fields_test": app_data["data"]}
     return knackpy_app
 
 
-def test_constructor_success(app_data):
-    app = knackpy.App(app_id=app_data["metadata"]["id"], metadata=app_data["metadata"])
+def test_constructor_success(app):
     assert app
 
 
@@ -36,25 +34,24 @@ def test_constructor_fail_missing_app_id(app, app_data):
         knackpy.App()
 
 
-def test_get_by_key(app_data):
-    app = knackpy.App(app_id=app_data["metadata"]["id"], metadata=app_data["metadata"])
+def test_get_by_key(app, app_data):
     # to side-load records, data[<key>] must exist in the app's container index
     # (ie, it has to exist in the app's metadata)
     app.data["object_3"] = app_data["data"]
     assert isinstance(app.records("object_3"), types.GeneratorType)
 
 
-def test_get_by_name(app_data):
-    app = knackpy.App(app_id=app_data["metadata"]["id"], metadata=app_data["metadata"])
+def test_get_by_dupe_name_fail(app, app_data):
     # to side-load records, data[<key>] must exist in the app's container index
     # (ie, it has to exist in the app's metadata)
     app.data["object_3"] = app_data["data"]
-    assert isinstance(app.records("all_fields_test"), types.GeneratorType)
+    with pytest.raises(knackpy.exceptions.exceptions.ValidationError):
+        assert isinstance(app.records("all_fields_test"), types.GeneratorType)
 
 
 def test_tzinfo(app_data):
     assert knackpy.App(
-        app_id=app_data["metadata"]["id"],
+        app_id=app_data["metadata"]["application"]["id"],
         metadata=app_data["metadata"],
         tzinfo="US/Eastern",
     )
