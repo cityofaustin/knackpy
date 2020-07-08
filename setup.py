8b2dae8eaf793a4d4b0c44ce4c77b03446ba67d0
John Clary <john.clary@austintexas.gov>
2020-07-08 07:43:33 -0500

    better setup

--- setup.py
+++ setup.py
@@ -3,18 +3,16 @@ from setuptools import setup
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-
 setup(
     author="John Clary",
     author_email="john.clary@austintexas.gov",
     classifiers=[
-        "Development Status :: 3 - Alpha",
+        "Development Status :: 2 - Beta",
         "Intended Audience :: Developers",
-        # Pick your license as you wish (should match "license" above)
         "License :: Public Domain",
         "Programming Language :: Python :: 3",
     ],
-    description="A Python API wrapper for interacting with Knack applications.",
+    description="A Python API client for interacting with Knack applications.",
     long_description=long_description,
     long_description_content_type="text/markdown",
     install_requires=["pytz", "requests"],
@@ -22,8 +20,7 @@ setup(
     license="Public Domain",
     name="knackpy",
     packages=["knackpy"],
-    test_suite="nose.collector",
-    tests_require=["nose"],
+    tests_require=["pytest"],
     url="http://github.com/cityofaustin/knackpy",
     version="1.0.0",
 )
