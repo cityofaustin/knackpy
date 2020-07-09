412c98c968fe0efa295ed4d14cbd453fe36d04e8
John Clary <john.clary@austintexas.gov>
2020-07-09 18:02:22 -0500

    use Field class. nix subfields

--- knackpy/fields.py
+++ knackpy/fields.py
@@ -1,3 +1,4 @@
+import collections
 from knackpy.models import FIELD_SETTINGS
 from . import utils, formatters
 
@@ -10,7 +11,12 @@ def set_field_def_views(field_defs, metadata):
     for scene in metadata["scenes"]:
         for view in scene["views"]:
             if view["type"] == "table":
-                field_keys = [column["field"]["key"] for column in view["columns"]]
+                # must ignore "link" columns, etc
+                field_keys = [
+                    column["field"]["key"]
+                    for column in view["columns"]
+                    if column.get("field")
+                ]
 
                 for key in field_keys:
                     field_defs[key].views.append(view["key"])
@@ -82,3 +88,61 @@ class FieldDef:
             self.formatter = getattr(formatters, self.type_)
         except AttributeError:
             self.formatter = getattr(formatters, "default")
+
+
+class Field(collections.abc.Container):
+    def __init__(self, key, data, field_def, timezone):
+        self.key = key
+        self.data = data
+        self.field_def = field_def
+        self.timezone = timezone
+
+    def __repr__(self):
+        if type(self.data) in [int, float]:
+            value_repr = self.data
+        else:
+            value_repr = f"'{self.data}'"
+        return f"<Field {{'{self.key}': {value_repr}}}>"
+
+    def __contains__(self, item):
+        if item in self.data:
+            return True
+
+    def format(self, format_labels=True, format_values=True):
+        """
+        Knack applies it's own standard formatting to values, which are always
+        available at the non-raw key. Knack includes the raw key in the dict
+        when formatting is applied, allowing access to the unformatted data.
+
+        Generally, the Knack formatting, where it exists, is fine. However there
+        are cases where we want to apply our own formatters, such datestamps,
+        (where the formatted value does not include a timezone offset), or
+        address fields, where we want to parse out the lat/lon properties as
+        subfields.
+
+        And there are still more cases, where we want to apply additional
+        formatting to the knack-formatted value, e.g. Timers.
+
+        See also: models.py, formatters.py.
+        """
+        value = self._format_value() if format_values else self.data
+        key = self.field_def.name if format_labels else self.field_def.key
+        return {key: value}
+
+    def _format_value(self):
+        kwargs = self._set_formatter_kwargs()
+
+        try:
+            return self.field_def.formatter(self.data, **kwargs)
+        except AttributeError:
+            # thrown when value is None
+            return self.data
+
+    def _set_formatter_kwargs(self):
+        # TODO: these should probably be field_def properties set from config
+        kwargs = {}
+
+        if self.field_def.type_ == "date_time":
+            kwargs["timezone"] = self.timezone
+
+        return kwargs
--- knackpy/records.py
+++ knackpy/records.py
@@ -1,4 +1,5 @@
 from . import utils
+from . import fields as _fields
 
 
 class Record:
@@ -11,83 +12,45 @@ class Record:
         self.identifier = identifier
         self.timezone = timezone
         self.raw = self._handle_record()
+        self.fields = self._handle_fields()
+
+    def _handle_fields(self):
+        fields = []
+
+        for field_def in self.field_defs:
+            key = field_def.key
+            key_raw = f"{key}_raw"
+            # we want to handle the raw data, except for a few types as defined by the
+            # "use_knack_format" property in models.py. raw values may not be present
+            # if the field is empty
+            value = (
+                self.raw[key_raw]
+                if not field_def.use_knack_format and self.raw[key] is not None
+                else self.raw[key]
+            )
+            field = _fields.Field(key, value, field_def, self.timezone)
+            fields.append(field)
+
+        return fields
 
     def _handle_record(self):
         record = self._replace_empty_strings(self.data)
         record = self._correct_knack_timestamp(record, self.timezone)
         return record
 
-    def format(self):
+    def format(self, format_labels=True, format_values=True):
+        record = {}
 
-        formatted_record = {"id": self.raw["id"]}
+        for field in self.fields:
+            formatted = field.format(
+                format_labels=format_labels, format_values=format_values
+            )
+            record.update(formatted)
 
-        for field_def in self.field_defs:
-
-            value = self._handle_value(self.raw, field_def)
-
-            key = field_def.name
-
-            if not field_def.subfields:
-                formatted_record.update({key: value})
-                continue
-
-            subfield_dict = self._handle_subfields(key, value, field_def.subfields)
-            formatted_record.update(subfield_dict)
-
-        return formatted_record
-
-    def _handle_value(self, record, field_def):
-        """
-        Knack applies it's own standard formatting to values, which are always
-        available at the non-raw key. Knack includes the raw key in the dict
-        when formatting is applied, allowing access to the unformatted data.
-
-        Generally, the Knack formatting, where it exists, is fine. However there
-        are cases where we want to apply our own formatters, such datestamps,
-        (where the formatted value does not include a timezone offset), or
-        address fields, where we want to parse out the lat/lon properties as
-        subfields.
-
-        And there are still more cases, where we want to apply additional
-        formatting to the knack-formatted value, e.g. Timers.
-        """
-
-        if field_def.use_knack_format:
-            return self._format_value(record.get(field_def.key), field_def)
-
-        value = record.get(f"{field_def.key}_raw")
-        return self._format_value(value, field_def)
-
-    def _handle_subfields(self, key, value, subfields):
-        try:
-            return {f"{key}_{subfield}": value.get(subfield) for subfield in subfields}
-
-        except AttributeError:
-            if value is None:
-                return {f"{key}_{subfield}": None for subfield in subfields}
-            else:
-                return {key: value}
+        return record
 
     def _replace_empty_strings(self, record):
         return {key: None if val == "" else val for key, val in record.items()}
-
-    def _format_value(self, value, field_def):
-        kwargs = self._set_formatter_kwargs(field_def)
-
-        try:
-            return field_def.formatter(value, **kwargs)
-        except AttributeError:
-            # thrown when value is None
-            return value
-
-    def _set_formatter_kwargs(self, field_def):
-        # TODO: these should probably be field_def properties set from config
-        kwargs = {}
-
-        if field_def.type_ == "date_time":
-            kwargs["timezone"] = self.timezone
-
-        return kwargs
 
     def _correct_knack_timestamp(self, record, timezone):
         # see note in knackpy.utils.correct_knack_timestamp
