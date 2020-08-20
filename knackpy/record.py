cc50727ac5c43e52c97d405f6a674ee37cfbc1a8
John Clary <john.clary@austintexas.gov>
2020-08-20 00:03:37 -0500

    correctly handle a nonetype case

--- knackpy/record.py
+++ knackpy/record.py
@@ -23,9 +23,7 @@ class Record(MutableMapping):
         self.immutable = True
 
     def __repr__(self):
-        identifier_value = self.data[self.identifier]
-        # if the identifier field has no value, use the record ID
-        identifier_value = identifier_value if identifier_value else self.data["id"]
+        identifier_value = self.data[self.identifier] if self.identifier else self.data["id"]
         return f"<Record '{identifier_value}'>"
 
     def __getitem__(self, client_key):
